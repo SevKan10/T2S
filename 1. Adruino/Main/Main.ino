dd7eb8c1d259344352fd8cc6f932bf49d645058c
KKhang <127233909+SevKan10@users.noreply.github.com>
2024-05-23 05:21:33 +0700

    change code
    thay đổi cho logic hơn

--- 1. Adruino/Main/Main.ino
+++ 1. Adruino/Main/Main.ino
@@ -1,24 +1,21 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
+#include <BlynkSimpleEsp32.h>
+#include <WiFi.h>
 
 #define i2c_Address 0x3C
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-/*=========OLED=========*/
 
 #define BLYNK_TEMPLATE_ID "TMPL6FBFJXTeP"
 #define BLYNK_TEMPLATE_NAME "T2S"
 #define BLYNK_AUTH_TOKEN "ew1TOzN0pqj0zEr1HK-6S2juw6HfvDHU"
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-/*---------BLYNK----------*/
 
-#define button_Correct 21 
-#define button_Start 19  
+#define button_Correct 21
+#define button_Start 19
 #define R_sti 27
 #define R_water 26
 #define R_phenol 25
@@ -30,30 +27,22 @@ char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "Xưởng 1";
 char pass[] = "xuong2dangxay";
 
-static float angle = 0.0;
-const int radius = 15;
-const int centerX = 64;
-const int centerY = 32;
-const int numDots = 7;
 int flagCorrect = 0;
 int flagStart = 0;
+int buttonBlynk;
 int Lux;
-/*=========Variable=========*/
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  WiFi.begin(ssid,pass);
+  WiFi.begin(ssid, pass);
   Blynk.begin(auth, ssid, pass);
   Wire.begin(23, 22);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) // Kiểm tra Wifi
-  {
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
     ESP.restart();
   }
-  if (!display.begin(i2c_Address, true)) // Kiểm tra màn hình 
-  {  
+  if (!display.begin(i2c_Address, true)) {
     Serial.println("SSD1306 allocation failed");
     for (;;);
   }
@@ -69,22 +58,15 @@ void setup()
   display.clearDisplay();
 
   unsigned long startMillis = millis();
-  while (millis() - startMillis < 7000) 
-  { 
+  while (millis() - startMillis < 7000) {
     display.setTextSize(1);
     display.setCursor(0, 0);
     display.println(" TITRA TECH STANDARD");
-    for (int i = 0; i < numDots; i++) 
-    {
-      float dotAngle = radians(angle + i * 360.0 / numDots);
-      int x = centerX + radius * cos(dotAngle);
-      int y = centerY + radius * sin(dotAngle);
+    for (int i = 0; i < 7; i++) {
+      float dotAngle = radians(i * 360.0 / 7);
+      int x = 64 + 15 * cos(dotAngle);
+      int y = 32 + 15 * sin(dotAngle);
       display.drawPixel(x, y, SH110X_WHITE);
-    }
-    angle += 10; 
-    if (angle >= 360.0) 
-    {
-      angle = 0.0;
     }
     display.display();
     delay(100);
@@ -97,8 +79,6 @@ void setup()
   display.display();
   display.clearDisplay();
 
-/*----------Loading effect--------*/
-
   pinMode(R_acid, OUTPUT);
   pinMode(R_base, OUTPUT);
   pinMode(R_phenol, OUTPUT);
@@ -107,273 +87,120 @@ void setup()
   pinMode(button_Correct, INPUT_PULLUP);
   pinMode(button_Start, INPUT_PULLUP);
 
-  digitalWrite(R_acid,0);
-  digitalWrite(R_base,0);
-  digitalWrite(R_phenol,0);
-  digitalWrite(R_water,0);
-  digitalWrite(R_sti,0);
-  ringBell(100,2);
-
+  digitalWrite(R_acid, 0);
+  digitalWrite(R_base, 0);
+  digitalWrite(R_phenol, 0);
+  digitalWrite(R_water, 0);
+  digitalWrite(R_sti, 0);
 }
 
-void loop()
-{
+void loop() {
+  Blynk.run();
   display.setTextSize(1);
   display.setCursor(0, 0);
   display.println(" TITRA TECH STANDARD");
   display.display();
 
-  if (digitalRead(button_Correct) == 0) // Hiệu chuẩn
-  {
+  if (buttonBlynk == 1) {
+    flagStart = 1;
+  } else if (buttonBlynk == 2) {
     String data = "!none!none!Correcting!";
-    Blynk.virtualWrite(V1, data); 
+    Blynk.virtualWrite(V1, data);
 
     display.setTextSize(1);
     display.setCursor(0, 30);
     display.println("Process: Correcting");
     display.display();
 
-    digitalWrite(R_acid,1);
-    digitalWrite(R_base,1);
-    digitalWrite(R_phenol,1);
+    digitalWrite(R_acid, 1);
+    digitalWrite(R_base, 1);
+    digitalWrite(R_phenol, 1);
     delay(20000);
     display.clearDisplay();
-    ringBell(100,2);
-
-    flagCorrect++;
-    if(flagCorrect >=2){flagCorrect = 0;}
+    flagCorrect = 1;
   }
-  switch(flagCorrect) // Giao diện nút hiệu chuẩn
-  {
+
+  if (digitalRead(button_Correct) == 0) {
+    String data = "!none!none!Correcting!";
+    Blynk.virtualWrite(V1, data);
+
+    display.setTextSize(1);
+    display.setCursor(0, 30);
+    display.println("Process: Correcting");
+    display.display();
+
+    digitalWrite(R_acid, 1);
+    digitalWrite(R_base, 1);
+    digitalWrite(R_phenol, 1);
+    delay(20000);
+    display.clearDisplay();
+    flagCorrect++;
+    if (flagCorrect >= 2) {
+      flagCorrect = 0;
+    }
+  }
+  switch (flagCorrect) {
     case 0:
       display.setTextSize(1);
       display.setCursor(0, 10);
       display.println("Correct:none");
       display.display();
-      
+
       display.setTextSize(1);
       display.setCursor(0, 20);
       display.println("Start  :none ");
       display.display();
 
-      display.setTextSize(1); 
+      display.setTextSize(1);
       display.setCursor(0, 30);
       display.println("Process:           ");
       display.display();
-    break;
-
+      break;
     case 1:
-      digitalWrite(R_acid,1);
-      digitalWrite(R_base,1);
-      digitalWrite(R_phenol,1);
+      digitalWrite(R_acid, 1);
+      digitalWrite(R_base, 1);
+      digitalWrite(R_phenol, 1);
 
       display.setTextSize(1);
       display.setCursor(0, 10);
       display.println("Correct:OK  ");
       display.display();
-      
+
       display.setTextSize(1);
       display.setCursor(0, 20);
       display.println("Start  :Ready ");
       display.display();
 
-      
-      display.setTextSize(1); 
+      display.setTextSize(1);
       display.setCursor(0, 30);
       display.println("Process:           ");
       display.display();
-    break;
+      break;
   }
 
- if (digitalRead(button_Start) == 0 && flagCorrect == 0)
-  {
+  if (digitalRead(button_Start) == 0 && flagCorrect == 0) {
     Serial.println("Please press correction button");
     display.clearDisplay();
-
     flagStart = 2;
-  }
-  else if (digitalRead(button_Start) == 0 && flagCorrect != 0 )
-  {
+  } else if (digitalRead(button_Start) == 0 && flagCorrect != 0) {
     Serial.println("Stating process titra");
     display.clearDisplay();
-
     flagStart = 1;
   }
 
-switch(flagStart) {
-  case 1: {
-    display.setTextSize(1);
-    display.setCursor(0, 0);
-    display.println(" TITRA TECH STANDARD");
-    display.display();
-
-    String data = "!HCl! !Titrating!"; //Acid
-    Blynk.virtualWrite(V1, data);
-
-    display.setTextSize(1); 
-    display.setCursor(0, 10);
-    display.println("Chemistry: HCl ");
-    display.display();
-    display.setTextSize(1);
-    display.setCursor(0, 30);
-    display.println("Process: Titrating");
-    display.display();
-    digitalWrite(R_base, LOW); 
-    digitalWrite(R_phenol, LOW); 
-
-    for(int i = 1; i < 10; i++) {
-      digitalWrite(R_acid, HIGH);
-      delay(250);
-      digitalWrite(R_acid, LOW);
-      delay(2000);
-    }
-    digitalWrite(R_acid, LOW); 
-    display.setTextSize(1);
-    display.setCursor(0, 20);
-    display.println("Volume: 100ml");
-    display.display();
-    String data1 = "!HCl!100ml!Titrating!"; //Acid
-    Blynk.virtualWrite(V1, data1);
-    delay(1000);
-
-    display.drawRect(15, 48, 100, 15, SH110X_WHITE); // hiệu ứng vẽ đường load
-    display.fillRect(15, 48, 25, 15, SH110X_WHITE);
-    display.display();
-
-    String data2 = "!Phenol! !Titrating!"; // Phenol
-    Blynk.virtualWrite(V1, data2);
-
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setCursor(0, 10);
-    display.println("Chemistry: Phenol");
-    display.display();
-    display.setTextSize(1);
-    display.setCursor(0, 30);
-    display.println("Process: Titrating");
-    display.display();
-    digitalWrite(R_acid, LOW); 
-    digitalWrite(R_base, LOW); 
-    digitalWrite(R_sti, HIGH); 
-
-    for(int i = 1; i < 3; i++) {
-      digitalWrite(R_phenol, HIGH);
-      delay(250);
-      digitalWrite(R_phenol, LOW);
-      delay(2000);
-    }                                     
-    digitalWrite(R_phenol, LOW);
-    display.setTextSize(1);
-    display.setCursor(0, 20);
-    display.println("Volume: 5ml");
-    display.display();
-    String data3 = "!Phenol!5ml!Titrating!"; // Phenol
-    Blynk.virtualWrite(V1, data3);
-    delay(1000);
-
-    display.drawRect(15, 48, 100, 15, SH110X_WHITE); // hiệu ứng vẽ đường load
-    display.fillRect(15, 48, 50, 15, SH110X_WHITE);
-    display.display();
-
-    String data4 = "!NaOH! !Titrating!"; // Base
-    Blynk.virtualWrite(V1, data4);
-
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setCursor(0, 10);
-    display.println("Chemistry: NaOH");
-    display.display();
-    display.setTextSize(1);
-    display.setCursor(0, 30);
-    display.println("Process: Titrating");
-    display.display();
-    digitalWrite(R_acid, LOW); 
-    digitalWrite(R_phenol, LOW); 
-
-    if (Lux <= 100) {
-      digitalWrite(R_base, HIGH);
-      delay(250);
-      digitalWrite(R_base, LOW);
-      delay(2000);
-    } else {
-      digitalWrite(R_acid, LOW);
-      digitalWrite(R_base, LOW);
-      digitalWrite(R_phenol, LOW);
-      digitalWrite(R_sti, LOW);
-    }
-    display.setTextSize(1);
-    display.setCursor(0, 20);
-    display.println("Volume: 110ml");
-    display.display();
-    String data5 = "!NaOH!110ml!Titrating!"; // Base
-    Blynk.virtualWrite(V1, data5);
-    delay(1000);
-
-    display.drawRect(15, 48, 100, 15, SH110X_WHITE); // hiệu ứng vẽ đường load
-    display.fillRect(15, 48, 75, 15, SH110X_WHITE);
-    display.display();
-
-    display.clearDisplay();
-    display.setTextSize(1); 
-    display.setCursor(0, 10);
-    display.println("Chemistry: ");
-    display.display();
-    display.setTextSize(1);
-    display.setCursor(0, 20);
-    display.println("Volume: ");
-    display.display();
-    display.setTextSize(1);
-    display.setCursor(0, 30);
-    display.println("Process: Calculating");
-    display.display();
-    /* thuật toán tính nồng độ CHCl.VHCl = CNaOH.VNaOH*/
-
-    display.drawRect(15, 48, 100, 15, SH110X_WHITE); // hiệu ứng vẽ đường load
-    display.fillRect(15, 48, 90, 15, SH110X_WHITE);
-    display.display();
-
-    // Đẩy dữ liệu sau khi tính toán
-    // Blynk.virtualWrite(V2, ); // thể tích !V1!V2!V3!
-    // Blynk.virtualWrite(V3, ); // nồng độ !C1!C2!C3!
-    // Blynk.virtualWrite(V4, ); // trung bình thể tích và nồng độ !Vavg!Cavg!
-
-    // Sau khi chuẩn độ xong bật motor nước
-    /*digitalWrite(R_acid, LOW);
-    digitalWrite(R_base, LOW);
-    digitalWrite(R_phenol, LOW);
-    digitalWrite(R_sti, LOW);
-    digitalWrite(R_water, HIGH); 
-    String data = "! ! !Washing!"; // Phenol
-    Blynk.virtualWrite(V1, data);
-
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setCursor(0, 10);
-    display.println("Chemistry:");
-    display.display();
-
-    display.setTextSize(1);
-    display.setCursor(0, 20);
-    display.println("Volume: ");
-    display.display();
-
-    display.setTextSize(1);
-    display.setCursor(0, 30);
-    display.println("Process: Washing");
-    display.display();*/
-
-    break;
-  }
-  case 2: {
-    display.setTextSize(1);
-    display.setCursor(0, 40);
-    display.println("Correction is none");
-    display.display();
-    delay(2000);
-    flagStart = 0;
-    flagCorrect = 0;
-    break;
+  switch (flagStart) {
+    case 1:
+      titrationProcess("HCl", 100, 10, R_acid);
+      titrationProcess("Phenol", 5, 3, R_phenol);
+      titrationProcess("NaOH", 110, 1, R_base);
+      break;
+    case 2:
+      display.setTextSize(1);
+      display.setCursor(0, 40);
+      display.println("Correction is none");
+      display.display();
   }
 }
 
-}
+
+
--- 1. Adruino/Main/secondFunction.ino
+++ 1. Adruino/Main/secondFunction.ino
@@ -6,3 +6,28 @@ void ringBell(int delayTimes, int repeatTimes)
     digitalWrite(buzzer, 0); delay(delayTimes);
   }
 }
+void titrationProcess(String chemistry, int volume, int repetitions, int pin) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0, 10);
+  display.println("Chemistry: " + chemistry);
+  display.display();
+  display.setCursor(0, 30);
+  display.println("Process: Titrating");
+  display.display();
+  for (int i = 1; i <= repetitions; i++) {
+    digitalWrite(pin, 1);
+    delay(250);
+    digitalWrite(pin, 0);
+    delay(2000);
+  }
+  display.setTextSize(1);
+  display.setCursor(0, 20);
+  display.println("Volume: " + String(volume) + "ml");
+  display.display();
+  Blynk.virtualWrite(V1, "!" + chemistry + "!" + String(volume) + "ml!Titrating!");
+  delay(1000);
+  display.drawRect(15, 48, 100, 15, SH110X_WHITE);
+  display.fillRect(15, 48, (volume / 100) * 75, 15, SH110X_WHITE);
+  display.display();
+}
